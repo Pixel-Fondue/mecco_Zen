a0ecdaf89a27e501e3053892d9ac1e410e8d628a
adamohern <adam@ChocolateChip.local>
2016-10-21 16:41:28 -0400

    cruft and selection fix

--- Scripts/zen_cameraSettings.py
+++ Scripts/zen_cameraSettings.py
@@ -7,5 +7,6 @@ def get_camera_id():
         renderCamera = "Camera"
     return lx.eval('query sceneservice item.ID ? {%s}' % renderCamera)
 
+lx.eval("select.drop item")
 lx.eval("select.subItem {%s} set" % get_camera_id())
 lx.eval("attr.formPopover {99877091315:sheet}")
