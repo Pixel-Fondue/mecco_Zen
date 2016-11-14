48263576dadc750884e0e8d098bc74caa326e131
adamohern <adam@ChocolateChip.local>
2016-11-14 10:58:43 -0500

    camera settings now works even if no render camera defined

--- Scripts/zen_cameraSettings.py
+++ Scripts/zen_cameraSettings.py
@@ -1,12 +1,27 @@
 #python
 
+import modo
+
 def get_camera_id():
     try:
         renderCamera = lx.eval("render.camera ?")
     except:
-        renderCamera = "Camera"
-    return lx.eval('query sceneservice item.ID ? {%s}' % renderCamera)
+        pass
+
+    cameras = modo.Scene().cameras
+
+    if len(cameras) == 0:
+        return None
+    elif len(cameras) >= 1:
+        lx.eval('render.camera {%s}' % cameras[0].id)
+        return cameras[0].id
 
 lx.eval("select.drop item")
-lx.eval("select.subItem {%s} set" % get_camera_id())
-lx.eval("attr.formPopover {99877091315:sheet}")
+
+camera_id = get_camera_id()
+
+if camera_id:
+    lx.eval("select.subItem {%s} set" % camera_id)
+    lx.eval("attr.formPopover {99877091315:sheet}")
+else:
+    modo.dialogs.alert("No Render Camera", "There is no render camera to select.")
