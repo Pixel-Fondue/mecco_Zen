dbcf3f9e386017828b282d0e62da587bc4750aa3
adamohern <adam@ChocolateChip.local>
2016-11-17 10:42:16 -0500

    base proficiency > advanced

--- lxserv/zen_doubleClick.py
+++ lxserv/zen_doubleClick.py
@@ -13,27 +13,11 @@ def selectedIsMesh():
 def setItemMode():
     lx.eval('select.typeFrom item;vertex;polygon;edge;pivot;center;ptag true')
 
-def selMode():
-    if lx.eval("select.typeFrom vertex;edge;polygon;item;pivot;center;ptag ?"):
-        return 'vertex'
-
-    elif lx.eval("select.typeFrom edge;vertex;polygon;item;pivot;center;ptag ?"):
-        return 'edge'
-
-    elif lx.eval("select.typeFrom polygon;vertex;edge;item;pivot;center;ptag ?"):
-        return 'polygon'
-
-    elif lx.eval("select.typeFrom ptag;vertex;polygon;item;pivot;center;edge ?"):
-        return 'ptag'
-
-    elif lx.eval("select.typeFrom item;vertex;polygon;edge;pivot;center;ptag ?"):
-        return 'item'
-
-    elif lx.eval("select.typeFrom pivot;vertex;polygon;item;edge;center;ptag ?"):
-        return 'pivot'
-
-    elif lx.eval("select.typeFrom center;vertex;polygon;item;pivot;edge;ptag ?"):
-        return 'center'
+def get_mode():
+    sel_svc = lx.service.Selection()
+    selitype = sel_svc.CurrentType (None)
+    seltype = sel_svc.LookupName (selitype)
+    return seltype
 
 
 class CMD_Zen_doubleClick(lxu.command.BasicCommand):
@@ -42,22 +26,25 @@ class CMD_Zen_doubleClick(lxu.command.BasicCommand):
         if not lx.eval('user.value mecco_zen.doubleClick ?'):
             return lx.symbol.e_OK
 
-        if selMode() == 'item':
+        mode = get_mode()
+        lx.out(mode)
+        
+        if mode in ('item', None, 'link'):
             if selectedIsMesh():
                 lx.eval("select.typeFrom polygon;vertex;edge;item;pivot;center;ptag true")
             else:
                 lx.eval("select.itemHierarchy")
-        elif selMode() == 'polygon':
+        elif mode == 'polygon':
             if lx.eval("query layerservice polys ? selected"):
                 lx.eval("select.connect")
             else:
                 setItemMode()
-        elif selMode() == 'edge':
+        elif mode == 'edge':
             if lx.eval("query layerservice edges ? selected"):
                 lx.eval("select.loop")
             else:
                 setItemMode()
-        elif selMode() == 'vertex':
+        elif mode == 'vertex':
             if lx.eval("query layerservice verts ? selected"):
                 lx.eval("select.connect")
             else:
