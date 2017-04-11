759e9b5838306b953c0da40587be8c2ec4bd4f6a
ArmanHunanyan <arman.hunanyan@gmail.com>
2017-04-11 18:52:05 +0400

    More work on zen
    More work on zen

--- Kits/mecco_Zen_10/lxserv/zen_uiOrientation.py
+++ Kits/mecco_Zen_10/lxserv/zen_uiOrientation.py
@@ -1,6 +1,6 @@
 # python
 
-import lx 
+import lx, lxifc, lxu
 from zen import CommanderClass
 
 CMD_NAME = 'zen.uiOrientation'
@@ -12,8 +12,8 @@ class CommandClass(CommanderClass):
                 {
                     'name': 'action',
                     'label': 'Action',
-                    'datatype': 'integer',
-                    'default': 0,
+                    'datatype': 'string',
+                    'default': 'toggle',
                     'values_list_type': 'popup',
                     'values_list': ['toggle', 'right', 'left'],
                     'flags': [],
@@ -32,21 +32,76 @@ class CommandClass(CommanderClass):
         if not self.commander_arg_value(0):
             return
 
-        if self.commander_arg_value(0) == 0:
+        if self.commander_arg_value(0) == "toggle":
             newVal = not lx.eval("user.value right_handed ?")
-        elif self.commander_arg_value(0) == 1:
+        elif self.commander_arg_value(0) == "right":
             newVal = True
         else:
             newVal = False
             
-        if newVal:
-            lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_mini_tag")
-            lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_miniFusion_tag")
-            lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_miniPaint_tag")
-            lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_miniSculpt_tag")
-        else:
-            lx.eval("viewport.hide False tag zen6_toolboxes_left_tag left zen6_toolboxes_tag_restore zen6_toolboxes_mini_tag")
+  #      if newVal:
+  #          lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_mini_tag")
+  #          lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_miniFusion_tag")
+  #          lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_miniPaint_tag")
+  #          lx.eval("viewport.hide False tag zen6_toolboxes_right_tag right zen6_toolboxes_tag_restore zen6_toolboxes_miniSculpt_tag")
+  #      else:
+  #          lx.eval("viewport.hide False tag zen6_toolboxes_left_tag left zen6_toolboxes_tag_restore zen6_toolboxes_mini_tag")
             
         lx.eval("user.value right_handed %s" %str(newVal))
+        
+        notifier = Notifier()
+        notifier.Notify(lx.symbol.fCMDNOTIFY_CHANGE_ALL)
+        
+    def cmd_Query(self, index, vaQuery):
+
+        # Create the ValueArray object
+        va = lx.object.ValueArray()
+        va.set(vaQuery)
+
+        if index != 1:
+            return lx.result.OK
+
+        # GATHER VALUES
+        # -------------
+
+        action = self.commander_arg_value(0)
+
+        lx.out("action = ", action)        
+        if action == "right":
+            va.AddInt(lx.eval("user.value right_handed ?"))
+        elif action == "left":
+            va.AddInt(not lx.eval("user.value right_handed ?"))
+            
+        return lx.result.OK
+        
+    def arg_UIValueHints(self, index):
+        return cmd_Notifiers()
+        
+    def cmd_Flags (self):
+        return lx.symbol.fCMD_UI | lx.symbol.fCMD_MODEL | lx.symbol.fCMD_UNDO
+        
+class cmd_Notifiers(lxu.command.BasicHints):
+
+    def __init__(self):
+        self._notifiers = [('zen.notifier',''),]
+        
+class Notifier(lxifc.Notifier):
+    masterList = {}
+
+    def noti_Name(self):
+        return "zen.notifier"
+
+    def noti_AddClient(self,event):
+        self.masterList[event.__peekobj__()] = event
+
+    def noti_RemoveClient(self,event):
+        del self.masterList[event.__peekobj__()]
+
+    def Notify(self, flags):
+        for event in self.masterList:
+            evt = lx.object.CommandEvent(self.masterList[event])
+            evt.Event(flags)
+
+lx.bless(Notifier, "zen.notifier")
 
 lx.bless(CommandClass, CMD_NAME)
